213ce55cac6072a6f9b5314d6efa5904770d7125
김민영 <2358006@mokwon.ac.kr>
2025-03-26 20:46:28 +0900

    time2

--- RollABall/Assets/Scripts/PlayerController.cs
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Mirror;
+using Mirror.BouncyCastle.Crypto.Modes;
 public class PlayerController : NetworkBehaviour
 {
     PlayerInputAction playerInput;
@@ -41,16 +42,61 @@ public class PlayerController : NetworkBehaviour
     private void Awake()
     {
         sceneScript = GameObject.FindObjectOfType<SceneScript>();
+
+        foreach (var item in weaponArray)
+        {
+            if (item != null)
+            {
+                item.SetActive(false);
+            }
+        }
     }
 
     void Update()
     {
+        Vector3 pos = this.transform.position;
+        if (!isLocalPlayer)
+        {
+            UpdateFloatingInfoPosition(pos);
+
+            UpdateWeaponPosition();
+            return;
+        }
+
         Vector3 movement = new Vector3(movementX, 0f, movementY) * MOVE_FORCE * Time.deltaTime;
         rb.AddForce(movement);
 
-        Vector3 pos = this.transform.position;
-        floatingInfo.transform.position = pos + Vector3.up * 1.5f;
+        UpdateFloatingInfoPosition(pos);
+        UpdateWeaponPosition();
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            var weapon = selectedWeaponLocal + 1;
+
+            if (weapon > weaponArray.Length)
+            {
+                selectedWeaponLocal = 1;
+            }
+            selectedWeaponLocal = weapon;
+            CmdChangeActiveWeapon(selectedWeaponLocal);
+        }
+        // Quaternion(4원수) : x, y, z, w
+        // Vector3의 x, y, z 와 다름
     }
+
+    void UpdateWeaponPosition()
+    {
+        weaponRoot.transform.position = this.transform.position;
+        var rot = this.transform.rotation.eulerAngles;
+
+        var currAngle = weaponRoot.transform.rotation.eulerAngles.y; // this.transform.rotation.eulerAngles.y;
+        var nextAngle = Mathf.Atan2(movementX, movementY);
+        var angle = Mathf.LerpAngle(currAngle, nextAngle, 3f * Time.deltaTime);
+        weaponRoot.transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        // Quaternion.Slerp(Quaternion.Euler(0f, currAngle * Mathf.Rad2Deg, 0f), Quaternion.Euler(0f, nextAngle * Mathf.Rad2Deg, 0f), Time.deltaTime);
+        Debug.Log($"curr angle {currAngle}, next angel {nextAngle}");
+    }
+
     #endregion
 
     #region other
@@ -84,11 +130,24 @@ public class PlayerController : NetworkBehaviour
         floatingInfo.transform.parent = null;
     }
 
+
     public override void OnStartClient()
     {
         if (!isLocalPlayer)
         {
             floatingInfo.transform.parent = null;
+            ClearWeaponRootParent();
+        }
+    }
+
+    void ClearWeaponRootParent()
+    {
+        foreach (var item in weaponArray)
+        {
+            if (item != null)
+            {
+                item.transform.parent = null;
+            }
         }
     }
 
@@ -120,4 +179,36 @@ public class PlayerController : NetworkBehaviour
     }
     #endregion
 
+    #region Weapon
+    int selectedWeaponLocal = 1;
+    public GameObject[] weaponArray;
+    public GameObject weaponRoot;
+
+    [SyncVar(hook = nameof(OnWeaponChanged))]
+    public int activeWeaponSynced = 1;
+
+    void OnWeaponChanged(int _Old, int _New)
+    {
+        if (0 < _Old && _Old < weaponArray.Length && weaponArray[_Old] != null)
+            weaponArray[_Old].SetActive(false);
+
+        if (0 < _New && _New < weaponArray.Length && weaponArray[_New] != null)
+            weaponArray[_New].SetActive(true);
+    }
+
+    [Command]
+
+    public void CmdChangeActiveWeapon(int newIndex)
+    {
+        activeWeaponSynced = newIndex;
+    }
+
+
+    #endregion
+
+    void UpdateFloatingInfoPosition(Vector3 pos)
+    {
+        floatingInfo.transform.position = pos + Vector3.up * 1.5f;
+        floatingInfo.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+    }
 }
